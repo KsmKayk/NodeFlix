1db63391edb6d6e122d88a487bd02fc3438a9e19
Kayk <ksm.kayk@outlook.com>
2021-12-27 12:33:56 -0300

    using knex to get all users in users api/json

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,20 +1,18 @@
 import {NextApiRequest, NextApiResponse} from "next"
+import knex from "../../../database"
 
 const handler = (req: NextApiRequest, res:NextApiResponse) => {
     try {
         const {method} = req
 
         if(method === "GET") {
-            res.status(200).json({
-                users: [
-                    {id: 1, name: "john doe"},
-                    {id: 2, name: "Janne doe"},
-                    {id: 3, name: "Example user"}
-                ]
-            })
-        }
 
-        res.status(405).json({message: "method not allowed"})
+            knex('users').then((results: Object) => {
+                res.status(200).json(results);
+            })
+        } else {
+            res.status(405).json({message: "method not allowed"})
+        }
 
     } catch (err) {
         // @ts-ignore
